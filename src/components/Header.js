37d10edf6102056ec94b54906969f31a17ef6300
Salome Ranson <salomeranson@gmail.com>
2021-07-07 13:58:28 -0400

    completed header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import reactDom from 'react-dom';
-
-
+import './Header.css';
 
 const Header = () => {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-dark navbar-default">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">Salome A. Ranson</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"></button>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">About</a>
+                            </li>
+
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Projects</a>
+                            </li>
+
+                            <li className="nav-item">
+                                 <a className="nav-link active" aria-current="page" href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+
                 </div>
             </nav>
+            
         </div>
     )
 }
