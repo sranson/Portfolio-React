1bdde08bee149d8385278fc6c7a1beba109c93d4
Salome Ranson <salomeranson@gmail.com>
2021-07-07 14:26:05 -0400

    completed styling for About page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import reactDom from 'react-dom';
 import Header from './Header';
-
+import Card from './Card';
 
 
 class App extends React.Component {
@@ -9,6 +9,7 @@ class App extends React.Component {
         return(
             <div>
                 <Header />
+                <Card />
             </div>
         )
     }
---
+++ src/components/Card.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Card.css';
+import Salome from '../Assets/Images/Salome.jpg';
+import { BackgroundColor } from 'jest-matcher-utils/node_modules/chalk';
+
+
+
+const Card = () => {
+    return (
+        <div style={{ paddingTop: "4%" }}>
+            <main className="container">
+                <body>
+                    <div>
+                        <div className="customCenterContent" style={{paddingTop: "3%"}}>
+                            <img src={Salome} alt="Photo of Salome" className="circle" />
+                        </div>
+                    </div>
+
+                    <div className="customCenterContent" style={{paddingBottom: "3%", paddingTop: "3%"}}>
+                        <h2>Software Developer | Lifelong Learner</h2>
+                    </div>
+
+                    <div className="customCenterContent">
+                        <h4>Hello, my name is Salome Ranson. I am a full-stack web developer based in Atlanta, GA.</h4>
+                    </div>
+
+                    <div className="customCenterContent" style={{paddingBottom: "3%"}}>
+                        <h5>Feel free to take a look at my latest projects on the <em><a href="#">My Work</a></em> Page.</h5>
+                    </div>
+                </body>
+            </main>
+        </div>
+    );
+};
+
+
+
+export default Card;
