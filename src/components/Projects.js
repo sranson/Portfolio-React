82587da1551719dad2a7fa45e0005cc956d572c9
Salome Ranson <salomeranson@gmail.com>
2021-07-07 15:59:21 -0400

    added three projects to the Projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,64 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Food from '../Assets/Images/food.jpg';
+import CoHoots from '../Assets/Images/CoHootsStock.jpg';
+import Blog from '../Assets/Images/blogPhoto.jpg';
+import './Projects.css'
+
 
 
 
 const Projects = () => {
     return(
         <div>
-        <h1>Projects</h1>
+            <main className="container">
+                <body>
+                    <section className="row customColor customSpacing" id="MyWork">
+                         <header>
+                            <h1 style={{textAlign: "center"}} >Recent Projects</h1>
+                        </header>
+
+                    <section className="col-md-4 col-lg-4 d-flex justify-content-center zoom">
+                        <div className="card cardSize">
+                            <Link to="https://sranson.github.io/recipe-finder/" target="_blank"><img className="card-img-top" src={Food} alt="photo of pasta" style={{height: "200px", width: "200px"}}/></Link>
+                            <div className="card-body">
+                                <h5 className="card-title">Recipe Finder</h5>
+                                <p className="card-text"><Link to="https://github.com/sranson/recipe-finder" target="_blank">Github Repository</Link></p>
+                                <p className="card-text"><Link to="https://sranson.github.io/recipe-finder/" target="_blank">Deployed Application</Link></p>
+                                <p><Link to="#" target="_blank">Screenshots</Link></p>
+                            </div>    
+                        </div>
+                    </section>
+
+
+                    <section className="col-md-4 col-lg-4 d-flex justify-content-center zoom">
+                        <div className="card cardSize">
+                            <Link to="https://cohoots.herokuapp.com/" target="_blank"><img className="card-img-top" src={CoHoots} alt="photo of pasta" style={{height: "200px", width: "200px"}} /></Link>
+                            <div className="card-body">
+                                <h5 className="card-title">CoHoots</h5>
+                                <p className="card-text"><Link to="https://github.com/klucas72/CoHoots" target="_blank">Github Repository</Link></p>
+                                <p className="card-text"><Link to="https://cohoots.herokuapp.com/" target="_blank">Deployed Application</Link></p>
+                                <p><Link to="#" target="_blank">Screenshots</Link></p>
+                            </div>    
+                        </div>
+                    </section>
+
+
+                    <section className="col-md-4 col-lg-4 d-flex justify-content-center zoom">
+                        <div className="card cardSize">
+                            <Link to="https://developer-blog-app.herokuapp.com/" target="_blank"><img className="card-img-top" src={Blog} alt="photo of pasta" style={{height: "200px", width: "200px"}} /></Link>
+                            <div className="card-body">
+                                <h5 className="card-title">CoHoots</h5>
+                                <p className="card-text"><Link to="https://github.com/sranson/Developer-Blog-App" target="_blank">Github Repository</Link></p>
+                                <p className="card-text"><Link to="https://developer-blog-app.herokuapp.com/" target="_blank">Deployed Application</Link></p>
+                                <p><Link to="#" target="_blank">Screenshots</Link></p>
+                            </div>    
+                        </div>
+                    </section>
+
+                    </section>
+                </body>
+            </main>
         </div>
     ) 
 }
