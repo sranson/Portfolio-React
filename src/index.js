f859b817367d20b5a3e6a56ac20a95805b7c386b
Salome Ranson <salomeranson@gmail.com>
2021-07-08 20:18:11 -0400

    added Router to main index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App';
+import { HashRouter as Router } from 'react-router-dom';
 
 
-ReactDom.render(<App />, document.querySelector('#root'));
+ReactDom.render(
+    <Router>
+        <App />
+    </Router>,
+    document.querySelector('#root')
+);
